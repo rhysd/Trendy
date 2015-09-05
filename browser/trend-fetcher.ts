01e34dbfcd1bbe5a1b9fd4a0d6cacc24eb6038b0
rhysd <lin90162@yahoo.co.jp>
2015-09-05 19:31:32 +0900

    add linter configs

---
+++ browser/trend-fetcher.ts
@@ -0,0 +1,38 @@
+import * as GHT from 'github-trend'
+import * as fs from 'fs';
+
+export default class TrendFetcher {
+    client: GHT.Client;
+
+    constructor(private renderer: GitHubElectron.WebContents, proxy?: string) {
+        this.client = new GHT.Client({proxy: proxy});
+    }
+
+    start() {
+        // TODO: Temporary
+        this.doScraping();
+    }
+
+    doScraping() {
+        // TODO: Temporary
+        fs.readFile('test.json', {encoding: 'utf8'}, (err, data) => {
+            if (err) {
+                const trendings = this.client.fetchTrending('').then(repos => {
+                    this.sendToRenderer(repos);
+                    fs.writeFile('test.json', JSON.stringify(repos, null, 2));
+                    console.log('sent!');
+                });
+            } else {
+                this.sendToRenderer(JSON.parse(data));
+            }
+        });
+    }
+
+    sendToRenderer(repos: Object[]): void {
+        this.renderer.send('repositories', repos);
+    }
+
+    stop(): void {
+        // TODO
+    }
+}
