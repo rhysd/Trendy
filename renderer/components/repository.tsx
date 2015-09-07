74792ecb9773623f20f5ef4903c6bfc59b5231c5
rhysd <lin90162@yahoo.co.jp>
2015-09-07 17:57:42 +0900

    add ExternalLink component

--- renderer/components/repository.tsx
+++ renderer/components/repository.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ExternalLink from './external-link';
 
 interface Props {
     repo: GitHubAPI.Repo;
@@ -19,7 +20,12 @@ export default class Repository extends React.Component<Props, {}> {
             <div className="repo">
                 <div className="secondary">
                     <div className="title">
-                        <span className={this.titleIcon()}/><a href={this.props.repo.owner.html_url}>{this.props.repo.owner.login}</a> / <a href={this.props.repo.html_url}>{this.props.repo.name}</a>
+                        <span className={this.titleIcon()}/>
+                        <ExternalLink url={this.props.repo.owner.html_url}>
+                            {this.props.repo.owner.login}
+                        </ExternalLink> / <ExternalLink url={this.props.repo.html_url}>
+                            {this.props.repo.name}
+                        </ExternalLink>
                     </div>
                     <div className="metadata">
                         <span className="counter"><span className="octicon octicon-star"/>{this.props.repo.stargazers_count}</span>
