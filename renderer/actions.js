46364aa555cdf3370f61dc21cddca6379f9b4a12
rhysd <lin90162@yahoo.co.jp>
2015-09-07 13:51:22 +0900

    remove old renderer implementation using babel

--- renderer/actions.js
+++
@@ -1,9 +0,0 @@
-import {ActionKind} from './constants';
-import Dispatcher from './dispatcher';
-
-export function updateRepos(repos) {
-    Dispatcher.dispatch({
-        type: ActionKind.UpdateRepos,
-        repos: repos
-    });
-}
--- renderer/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Root from './components/root.jsx';
-
-React.render(
-        <Root />,
-        document.body
-    );
-
