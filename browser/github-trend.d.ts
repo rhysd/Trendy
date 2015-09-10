d7e4211174866ab7d7f248315c959e5c00c4df98
rhysd <lin90162@yahoo.co.jp>
2015-09-10 19:48:44 +0900

    update github-trend.d.ts following 0.0.3

--- browser/github-trend.d.ts
+++ browser/github-trend.d.ts
@@ -1,32 +1,46 @@
 declare module 'github-trend' {
-    export interface Repository {
+    export interface RepositoryEntry {
         owner: string;
         name: string;
+    }
+
+    interface Repository {
+        [param: string]: any;
+    }
+
+    interface Repositories {
+        [lang: string]: Repository[];
+    }
+
+    interface Language {
+        color: string;
+        aliases?: string[];
+    }
+
+    interface Languages {
+        [lang: string]: Language;
     }
 
     export interface ScraperConfig {
         proxy?: string;
     }
 
-    export interface Language {
-        color: string;
-        aliases? : string[];
-    }
-
     export class Scraper {
         constructor(config?: ScraperConfig);
         fetchTrendPage(lang: string): Promise<string>;
-        scrapeTrendingRepos(lang: string): Promise<Repository[]>;
-        fetchLanguageYAML(): Promise<Object>;
-        scrapeLanguageColors(): Promise<Object>;
-        scrapeLanguageNames(): string[];
+        scrapeTrendingRepos(lang: string): Promise<RepositoryEntry[]>;
+        fetchLanguageYAML(): Promise<Languages>;
+        scrapeLanguageColors(): Promise<{[lang: string]: string}>;
+        scrapeLanguageNames(): Promise<string[]>;
     }
 
     export class Client {
         constructor(config?: ScraperConfig);
-        fetchGetAPI(repo: Repository): Promise<Object>;
-        fetchTrending(lang: string): Promise<Object[]>;
-        fetchTrendings(langs: string[]): Promise<Object>;
+        fetchGetAPI(repo: RepositoryEntry): Promise<Repository>;
+        fetchTrending(lang: string): Promise<Repository[]>;
+        fetchTrendings(langs: string[]): Promise<Repositories>;
+        fetchTrendingWithReadme(lang: string): Promise<Repository[]>;
+        fetchTrendingsWithReadme(langs: string[]): Promise<Repositories>;
     }
 }
 
