ebe11f0c58437540f57d33174b1707b2303a51d5
rhysd <lin90162@yahoo.co.jp>
2015-09-14 04:15:19 +0900

    add login window

---
+++ browser/authentication.ts
@@ -0,0 +1,99 @@
+import * as BrowserWindow from 'browser-window';
+import * as fs from 'fs';
+import * as request from 'request';
+import * as querystring from 'querystring';
+
+const CLIENT_ID = '582b0dbcdf7ee3de467d';
+const CLIENT_SECRET = 'b2723dfdc53728bff30f07912698237718ba82a6';
+
+export default class Authentication {
+    constructor(public tokens_path: string) {
+    }
+
+    authenticate(code: string) {
+        return new Promise((resolve, reject) => {
+            console.log('code: ' + code);
+            request.post({
+                    url: 'https://github.com/login/oauth/access_token',
+                    form: {
+                        client_id: '582b0dbcdf7ee3de467d',
+                        client_secret: 'b2723dfdc53728bff30f07912698237718ba82a6',
+                        code: code,
+                    },
+                },
+                (err, res, body) => {
+                    if (err) {
+                        console.log('POST code error: ' + err.message);
+                        reject(err);
+                        return;
+                    }
+
+                    if (res.statusCode !== 200) {
+                        console.log('POST code error: ' + res.statusCode);
+                        reject(new Error('Invalid response: ' + res.statusCode));
+                        return;
+                    }
+
+                    const parsed = querystring.parse(body);
+                    if (!parsed.access_token) {
+                        console.log('Body parsing error: ' + body);
+                        reject(new Error('Invalid body: ' + body));
+                    }
+
+                    console.log('Login: Success!');
+
+                    fs.writeFile(this.tokens_path, JSON.stringify({github: parsed.access_token}), {encoding: 'utf8'});
+
+                    resolve(parsed.access_token);
+                }
+            );
+        });
+    }
+
+    login() {
+        return new Promise((resolve, reject) => {
+            let auth_win = new BrowserWindow({
+                width: 800,
+                height: 600,
+                'node-integration': false,
+                'web-preferences': {
+                    'node-integration': false,
+                    'web-security': true,
+                }
+            });
+
+            auth_win.on('close', function(){ auth_win = null; });
+
+            auth_win.webContents.on('will-navigate', (event: Event, url: string) => {
+                const raw_code = /code=([^&]*)/.exec(url) || null;
+                const code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+                const error = /\?error=(.+)$/.exec(url);
+
+                if (error) {
+                    console.log('login failed!');
+                    setTimeout(() => auth_win.close(), 0);
+                    reject(error);
+                } else if (code) {
+                    setTimeout(() => auth_win.close(), 0);
+                    resolve(code);
+                }
+            });
+
+            const url = 'https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID;
+            auth_win.loadUrl(url);
+        }).then(this.authenticate.bind(this));
+    }
+
+    getToken() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.tokens_path, {encoding: 'utf8'}, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    try { resolve(JSON.parse(data).github); }
+                    catch(e) { reject(e); }
+                }
+            });
+        });
+    }
+}
--- browser/main.ts
+++ browser/main.ts
@@ -6,6 +6,8 @@ import * as Tray from 'tray';
 import * as Menu from 'menu';
 import TrendFetcher from './trend-fetcher';
 import Config from './config';
+import Auth from './authentication';
+
 
 global.config = new Config(path.join(app.getPath('userData'), 'config.json'));
 const app_config = global.config.load();
@@ -13,7 +15,7 @@ const app_config = global.config.load();
 const normal_icon = path.join(__dirname, '..', '..', 'resource', 'trayicon', app_config.icon_color === 'white' ? 'graph_white.png' : 'graph.png');
 const notified_icon =path.join(__dirname, '..', '..', 'resource', 'trayicon', 'graph_notify.png');
 const index_html = 'file://' + path.join(__dirname, '..', '..', 'index.html');
-
+const auth = new Auth(path.join(app.getPath('userData'), 'tokens.json'));
 
 function startMenubarApp() {
     let menuConfig = {
@@ -30,6 +32,9 @@ function startMenubarApp() {
     menu_window.on('after-create-window', () => {
         menu_window.tray.setToolTip('Show Menu Window');
         let fetcher = new TrendFetcher(menu_window.window.webContents, app_config.languages);
+
+        // TODO: Set auth token if available
+
         ipc.on('renderer-ready', () => fetcher.start());
         ipc.on('force-update-repos', () => fetcher.doScraping());
         ipc.on('tray-icon-normal', () => menu_window.tray.setImage(normal_icon));
@@ -49,6 +54,8 @@ function startIsolatedApp() {
         let fetcher = new TrendFetcher(win.webContents, app_config.languages);
         ipc.on('renderer-ready', () => fetcher.start());
         ipc.on('force-update-repos', () => fetcher.doScraping());
+
+        // TODO: Set auth token if available
 
         let app_icon = new Tray(normal_icon);
         const context_menu = Menu.buildFromTemplate([
